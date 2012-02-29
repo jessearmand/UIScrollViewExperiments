12c26076eef8be100df9d6ea8952ea55f589d917
Jesse Armand <armand.jesse@gmail.com>
2012-03-01 01:32:49 +0800

    Cleaned up indentation...

--- UIScrollView-RunLoopExperiments/UIScrollView-RunLoopExperiments/UISVRLERootViewController.m
+++ UIScrollView-RunLoopExperiments/UIScrollView-RunLoopExperiments/UISVRLERootViewController.m
@@ -45,9 +45,9 @@ static NSString *const FlickrInterestingnessMethod = @"flickr.interestingness.ge
 	self = [super init];
 	if (!self)
 		return nil;
-
-  self.page = 1;
-  self.photos = [NSMutableArray arrayWithCapacity:0];  
+	
+	self.page = 1;
+	self.photos = [NSMutableArray arrayWithCapacity:0];  
   
 	self.rlObserver = CFRunLoopObserverCreateWithHandler(NULL, kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
 	
@@ -121,8 +121,8 @@ static NSString *const FlickrInterestingnessMethod = @"flickr.interestingness.ge
 				return;
 			
 			NSLog(@"back to default mode, time to do work");
-      
-      [self flickrGetInterestingness];
+			
+			[self flickrGetInterestingness];
 			
 		});
 		
@@ -133,91 +133,91 @@ static NSString *const FlickrInterestingnessMethod = @"flickr.interestingness.ge
 }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  
-  self.cellNib = [UINib nibWithNibName:NSStringFromClass([UISVRLEPhotoTableViewCell class]) bundle:nil];
-  
-  self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+	[super viewDidLoad];
+	
+	self.cellNib = [UINib nibWithNibName:NSStringFromClass([UISVRLEPhotoTableViewCell class]) bundle:nil];
+	
+	self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
 - (void) updateViews {
-  if ([self.photos count] > 0)
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
-  
-  [self.tableView reloadData];
+	if ([self.photos count] > 0)
+		self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+	
+	[self.tableView reloadData];
 }
 
 #pragma mark - Flickr API
 
 - (NSURL *) flickrPhotoURLFromDictionary:(NSDictionary *)photoDict {
   
-  //
-  // URL format: http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-  //
-  // photo json object: { "id": "6938992897", "owner": "36621592@N06", "secret": "66e7f23f1c", "server": "7042", "farm": 8, "title": "Contigo - with you", "ispublic": 1, "isfriend": 0, "isfamily": 0 }
-  //
-  
-  NSString *photoId = [photoDict objectForKey:@"id"];
-  NSString *photoSecret = [photoDict objectForKey:@"secret"];
-  NSString *serverId = [photoDict objectForKey:@"server"];
-  NSNumber *farmId = [photoDict objectForKey:@"farm"];
-  NSString *photoURLString = [NSString stringWithFormat:@"http://farm%@.staticflickr.com/%@/%@_%@_z.jpg", farmId, serverId, photoId, photoSecret];
-  
-  NSURL *photoURL = [NSURL URLWithString:photoURLString];
-  return photoURL;
+	//
+	// URL format: http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+	//
+	// photo json object: { "id": "6938992897", "owner": "36621592@N06", "secret": "66e7f23f1c", "server": "7042", "farm": 8, "title": "Contigo - with you", "ispublic": 1, "isfriend": 0, "isfamily": 0 }
+	//
+	
+	NSString *photoId = [photoDict objectForKey:@"id"];
+	NSString *photoSecret = [photoDict objectForKey:@"secret"];
+	NSString *serverId = [photoDict objectForKey:@"server"];
+	NSNumber *farmId = [photoDict objectForKey:@"farm"];
+	NSString *photoURLString = [NSString stringWithFormat:@"http://farm%@.staticflickr.com/%@/%@_%@_z.jpg", farmId, serverId, photoId, photoSecret];
+	
+	NSURL *photoURL = [NSURL URLWithString:photoURLString];
+	return photoURL;
 }
 
 - (void) flickrGetInterestingness {
   
-  [SVProgressHUD showWithStatus:@"Loading.." networkIndicator:YES];
-  
-  // Set the page limit to 10, so it will be loading 10*20 = 200 photos in the end
-  
-  ++self.page;
-  
-  if (self.page > 10) {  
-    self.page = 1;
-    return;
-  }
-  
-  NSURL *baseURL = [NSURL URLWithString:FlickrAPIBaseURL];
-  AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
-  
-  NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithCapacity:0];
-  [parameters setObject:FlickrInterestingnessMethod forKey:@"method"];
-  [parameters setObject:FlickrAPIKey forKey:@"api_key"];
-  [parameters setObject:@"json" forKey:@"format"];
-  [parameters setObject:[NSNumber numberWithInt:1] forKey:@"nojsoncallback"];
-  [parameters setObject:[NSNumber numberWithInt:page] forKey:@"page"];
-  [parameters setObject:[NSNumber numberWithInt:20] forKey:@"per_page"];
-  
-  NSURLRequest *request = [httpClient requestWithMethod:@"GET" path:@"/services/rest" parameters:parameters];
-  AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-    NSLog(@"%@", JSON);
-    
-    if ([response statusCode] == 200) {
-      NSDictionary *photosDict = [JSON objectForKey:@"photos"];
-      if (photosDict != nil) {        
-        NSArray *photosArray = [photosDict objectForKey:@"photo"];
-        for (NSDictionary *photoDict in photosArray) {
-          NSURL *photoURL = [self flickrPhotoURLFromDictionary:photoDict];
-          NSString *photoTitle = [photoDict objectForKey:@"title"];
-          
-          if ( (photoURL != nil) && (photoTitle.length > 0) ) {
-            NSDictionary *photo = [NSDictionary dictionaryWithObjectsAndKeys:photoURL, @"url", photoTitle, @"title", nil];
-            if (photo != nil) [self.photos addObject:photo];
-          }
-          
-          [self updateViews];
-        }
-      }
-    }
-
-  } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-    NSLog(@"url: %@, error: %@, %@", [request URL], [error userInfo], JSON);
-  }];
-  
-  [httpClient enqueueHTTPRequestOperation:operation];
+	[SVProgressHUD showWithStatus:@"Loading.." networkIndicator:YES];
+	
+	// Set the page limit to 10, so it will be loading 10*20 = 200 photos in the end
+	
+	++self.page;
+	
+	if (self.page > 10) {  
+		self.page = 1;
+		return;
+	}
+	
+	NSURL *baseURL = [NSURL URLWithString:FlickrAPIBaseURL];
+	AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
+	
+	NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithCapacity:0];
+	[parameters setObject:FlickrInterestingnessMethod forKey:@"method"];
+	[parameters setObject:FlickrAPIKey forKey:@"api_key"];
+	[parameters setObject:@"json" forKey:@"format"];
+	[parameters setObject:[NSNumber numberWithInt:1] forKey:@"nojsoncallback"];
+	[parameters setObject:[NSNumber numberWithInt:page] forKey:@"page"];
+	[parameters setObject:[NSNumber numberWithInt:20] forKey:@"per_page"];
+	
+	NSURLRequest *request = [httpClient requestWithMethod:@"GET" path:@"/services/rest" parameters:parameters];
+	AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+		NSLog(@"%@", JSON);
+		
+		if ([response statusCode] == 200) {
+			NSDictionary *photosDict = [JSON objectForKey:@"photos"];
+			if (photosDict != nil) {        
+				NSArray *photosArray = [photosDict objectForKey:@"photo"];
+				for (NSDictionary *photoDict in photosArray) {
+					NSURL *photoURL = [self flickrPhotoURLFromDictionary:photoDict];
+					NSString *photoTitle = [photoDict objectForKey:@"title"];
+					
+					if ( (photoURL != nil) && (photoTitle.length > 0) ) {
+						NSDictionary *photo = [NSDictionary dictionaryWithObjectsAndKeys:photoURL, @"url", photoTitle, @"title", nil];
+						if (photo != nil) [self.photos addObject:photo];
+					}
+					
+					[self updateViews];
+				}
+			}
+		}
+		
+	} failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+		NSLog(@"url: %@, error: %@, %@", [request URL], [error userInfo], JSON);
+	}];
+	
+	[httpClient enqueueHTTPRequestOperation:operation];
 }
 
 #pragma mark - UIViewController 
@@ -230,14 +230,14 @@ static NSString *const FlickrInterestingnessMethod = @"flickr.interestingness.ge
 
 - (void) scrollViewDidScroll:(UIScrollView *)scrollView {
 
-//	NSLog(@"%s %@", __PRETTY_FUNCTION__, scrollView);
+	//	NSLog(@"%s %@", __PRETTY_FUNCTION__, scrollView);
 
 }
 
 #pragma mark - UITableView
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-  return 320.f;
+	return 320.f;
 }
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -247,27 +247,27 @@ static NSString *const FlickrInterestingnessMethod = @"flickr.interestingness.ge
 
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-  NSString *identifier = NSStringFromClass([UISVRLEPhotoTableViewCell class]);
+	NSString *identifier = NSStringFromClass([UISVRLEPhotoTableViewCell class]);
 	UISVRLEPhotoTableViewCell *cell = (UISVRLEPhotoTableViewCell *)[tableView dequeueReusableCellWithIdentifier:identifier];
 	if (cell == nil) {
-    NSArray *nibObjects = [self.cellNib instantiateWithOwner:self options:nil];
-    for (id nibObject in nibObjects) {
-      if ([nibObject isKindOfClass:[UISVRLEPhotoTableViewCell class]]) {
-        cell = nibObject;
-        break;
-      }
-    }
+		NSArray *nibObjects = [self.cellNib instantiateWithOwner:self options:nil];
+		for (id nibObject in nibObjects) {
+			if ([nibObject isKindOfClass:[UISVRLEPhotoTableViewCell class]]) {
+				cell = nibObject;
+				break;
+			}
+		}
 	}
 	
-  if ([self.photos count] > indexPath.row) {
-    NSDictionary *photo = [self.photos objectAtIndex:indexPath.row];
-    
-    NSURL *photoURL = [photo objectForKey:@"url"];
-    NSString *photoTitle = [photo objectForKey:@"title"];
-    
-    [cell.photoImageView setImageWithURL:photoURL];
-    cell.photoTitleLabel.text = photoTitle;
-  }
+	if ([self.photos count] > indexPath.row) {
+		NSDictionary *photo = [self.photos objectAtIndex:indexPath.row];
+		
+		NSURL *photoURL = [photo objectForKey:@"url"];
+		NSString *photoTitle = [photo objectForKey:@"title"];
+		
+		[cell.photoImageView setImageWithURL:photoURL];
+		cell.photoTitleLabel.text = photoTitle;
+	}
 	
 	return cell;
 
